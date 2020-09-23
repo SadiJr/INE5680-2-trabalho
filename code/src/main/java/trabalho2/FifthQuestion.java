3d1b5de2fe9f3f8a7a8a6d795a38e4e0c9e26446
SadiJr <sadijacinto@gmail.com>
2020-09-23 17:51:55 -0300

    Init fifth question. Decrypt with errors

--- code/src/main/java/trabalho2/FifthQuestion.java
+++ code/src/main/java/trabalho2/FifthQuestion.java
@@ -1,5 +1,77 @@
 package trabalho2;
 
-public class FifthQuestion {
+import java.nio.ByteBuffer;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Scanner;
 
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Hex;
+
+public class FifthQuestion {
+	private static final FifthQuestion obj = new FifthQuestion();
+
+	private String encrypt(String password, String message) throws Exception {
+		String salt = generateSalt();
+
+		Cipher c = Cipher.getInstance("AES/GCM/NoPadding");
+		c.init(Cipher.ENCRYPT_MODE, generateKey(password, salt), new GCMParameterSpec(128, salt.getBytes()));
+
+		return new String(Hex.encodeHex(c.doFinal(message.getBytes())));
+	}
+
+	private Key generateKey(String password, String salt) throws Exception {
+		SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+		SecretKey derivedKey = factory
+				.generateSecret(new PBEKeySpec(password.toCharArray(), salt.getBytes(), 10000, 256));
+		return new SecretKeySpec(derivedKey.getEncoded(), "AES");
+	}
+
+	private String decrypt(String password, String message)
+			throws InvalidKeyException, InvalidAlgorithmParameterException, Exception {
+		String salt = generateSalt();
+
+		Cipher c = Cipher.getInstance("AES/GCM/NoPadding");
+		c.init(Cipher.DECRYPT_MODE, generateKey(password, salt), new GCMParameterSpec(128, salt.getBytes()));
+		c.update(Hex.decodeHex(message));
+		return new String(Hex.encodeHex(c.doFinal(Hex.decodeHex(message))));
+	}
+
+	private String generateSalt() throws NoSuchAlgorithmException {
+		SecureRandom sr = SecureRandom.getInstanceStrong();
+		byte[] salt = new byte[96];
+		sr.nextBytes(salt);
+		return Hex.encodeHexString(salt);
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		try {
+			System.out.println("Digite algo:");
+			String message = input.nextLine();
+
+			System.out.println("Digite a senha:");
+			String password = input.nextLine();
+
+			String encrypt = obj.encrypt(password, message);
+			System.out.println("Mensagem criptografada: " + encrypt);
+
+			String decrypt = obj.decrypt(password, encrypt);
+			System.out.println("Mensagem decriptografada: " + decrypt);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// TODO: handle finally clause
+		}
+	}
 }
